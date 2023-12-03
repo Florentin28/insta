7932a198277ea798d8e6fefe084be47934622d38
Florentin28 <muraillef@gmail.com>
2023-12-03 22:22:46 +0100

    design welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
         <h1 class="font-bold text-3xl mb-8 mt-24">Bienvenue sur Instagram</h1>
 
         <div class="flex flex-col items-center space-y-4">
-            <a href="{{ route('register') }}" class="bg-purple-600 text-white px-4 py-2 rounded-md transition-transform hover:scale-105 focus:outline-none focus:ring focus:border-custom-focus-color duration-300">
+            <a href="{{ route('register') }}" class="bg-purple-600 text-white px-4 py-2 rounded-md border border-width-2 border-black transition-transform hover:scale-105 focus:outline-none focus:ring focus:border-custom-focus-color duration-300">
                 Créer un compte
             </a>
-            <a href="{{ route('login') }}" class="bg-purple-600 text-white px-4 py-2 rounded-md transition-transform hover:scale-105 focus:outline-none focus:ring focus:border-custom-focus-color duration-300">
+            <a href="{{ route('login') }}" class="bg-purple-600 text-white px-4 py-2 rounded-md border border-width-2 border-black transition-transform hover:scale-105 focus:outline-none focus:ring focus:border-custom-focus-color duration-300">
                 Se connecter
             </a>
         </div>
